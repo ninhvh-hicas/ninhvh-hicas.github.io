80c7bf09a7409f825486d51cba19d1d29dbd3e4f
ninhvh-hicas <ninh_vh@hicas.com.vn>
2020-12-07 11:45:00 +0700

    style css sample

--- React/react-style-lession1/src/App.js
+++ React/react-style-lession1/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import style from './App.module.css';
+import Inline from './components/inline';
+import StyleSheet from './components/style-sheet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyleSheet primary={true}/>
+      <Inline/>
+      <h1 className="error">Error</h1>
+      <h1 className={style.sucess}>Sucess</h1>
     </div>
   );
 }
---
+++ React/react-style-lession1/src/components/style-sheet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style-sheet.css';
+
+function StyleSheet(props){
+    let className = props.primary ? 'primary' : '';
+    return (
+        <div>
+            <h1 className={`${className} font-xl`}>Style Sheets</h1>
+        </div>
+    )
+}
+export default StyleSheet;
