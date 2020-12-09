5962a7a4811d4afb8ba95321bd1e099dcb48cd1e
ninhvh-hicas <ninh_vh@hicas.com.vn>
2020-12-09 11:38:13 +0700

    redux sample

---
+++ React/react-store-lession/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import VisibilityFilters from "./components/VisibilityFilters";
+
+
+function App() {
+  return (   
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <AddTodo />
+      <TodoList />
+      <VisibilityFilters />
+    </div>
+  );
+}
+
+export default App;
